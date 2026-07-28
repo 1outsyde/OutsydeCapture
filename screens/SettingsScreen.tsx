b0024fb77982ab47acde27de3ad8402fbdf4d6b0
1outsyde <info@goutsyde.com>
2026-07-28 17:23:44 -0400

    Increase paddingTop in contentContainer style

--- screens/SettingsScreen.tsx
+++ screens/SettingsScreen.tsx
@@ -253,7 +253,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#141414",
   },
   contentContainer: {
-  paddingTop: 16,      // ← add this line
+  paddingTop: 24,      // ← add this line
   paddingBottom: 40,
 },
   sectionLabel: {
